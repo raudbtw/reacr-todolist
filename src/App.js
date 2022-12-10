9c8e9f8f178aed9392644b6fd6d290edc40b0f4e
raudbtw <max.yatoff@gmail.com>
2022-12-10 18:24:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { v4 } from 'uuid'
+import ToDo from './Components/ToDo';
+import ToDoForm from './Components/ToDoForm';
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTask = (userInput) => {
+    if(userInput) {
+      const newItem = {
+        id: v4(),
+        task: userInput,
+        isDone: false
+      }
+      setTodos([...todos, newItem])
+    }
+  }
+
+  const removeTask = (id) => {
+    setTodos([...todos.filter((todo) => todo.id !== id)])
+  }
+
+  const handleToggle = (id) => {
+    setTodos([
+      ...todos.map((todo) => 
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : {...todo }
+      )
+    ])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Список задач: {todos.length}</h1>
       </header>
+      <ToDoForm addTask={addTask} />
+      {todos.map((todo) => {
+        return (
+          <ToDo
+            todo={todo}
+            key={todo.id}
+            toggleTask={handleToggle}
+            removeTask={removeTask}
+            />
+        )
+      })}
     </div>
   );
 }
